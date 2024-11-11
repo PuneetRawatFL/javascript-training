6169309a3fad79368ca6dabbaa153310bb6cd581
PuneetRawatFL <puneet.rawat@fexle.com>
2024-11-11 17:54:37 +0530

    async await

--- script4.js
+++ script4.js
@@ -31,24 +31,24 @@
 
 //Promise examples
 //pending promise
-let p1 = new Promise((res, rej) => {
+var p1 = new Promise((res, rej) => {
     console.log("Pending Promise");
 });
 
 //fulfilled promise
-let p2 = new Promise((res, rej) => {
+var p2 = new Promise((res, rej) => {
     console.log("Fulfilled Promise");
     res("Successful");
 });
 
 //rejected promise
-let p3 = new Promise((res, rej) => {
+var p3 = new Promise((res, rej) => {
     console.log("Rejected");
     rej("error resolving");
 });
 
-//handling promises using .then() & .catch()
-let p4 = new Promise((res, rej) => {
+//Handling promises using .then() & .catch()
+var p4 = new Promise((res, rej) => {
     console.log("New promise");
     res("successful");
     // rej("rejected");
@@ -62,7 +62,72 @@ p4.catch(() => {
     console.log("method rejected");
 });
 
+// Promise chaining
+
+// var p1 = new Promise(function (res, rej) {
+//     return res("Promise 1 resolved");
+// });
+
+// var p2 = p1.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res, rej) {
+//         return res("Promise 2 resolved");
+//     });
+// });
+
+// var p3 = p2.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res, rej) {
+//         return res("Promise 3 resolved");
+//     });
+// });
+
+// p3.then(function (data) {
+//     console.log(data);
+// });
+
+// var p4 = new Promise((res, rej) => {
+//     var success = false;
+//     setTimeout(() => {
+//         if (success) {
+//             return res("Promise 4 resolved");
+//         } else {
+//             return rej("error occured while resolving promise");
+//         }
+//     }, 2000);
+// });
+
+// p4.then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+// function abcd() {
+//     fetch(`https://randomuser.me/api/`)
+//         .then(function (raw) {
+//             return raw.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+// }
+// abcd();
+
 // ASYNC AWAIT
+function api() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("api data");
+            res(200);
+        }, 2000);
+    });
+}
+
+async function getApi() {
+    await api();
+}
+
 async function abcd2() {
     let raw = await fetch("https://randomuser.me/api/");
     let ans = await raw.json();
